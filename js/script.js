c6841cc7d008f8c2e283e26ff888b45d7ae80173
DorinVieru <dorino.vieru@gmail.com>
2023-11-17 16:27:50 +0100

    [UPDATE] Improve code js & finish project

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let nome = prompt("Inserisci il tuo nome");
 let cognome = prompt("Inserisci il tuo cognome");
 let colore = prompt("Inserisci il tuo colore preferito");
 
-let benvenuto = `Ciao ${nome} ${cognome} ${colore} 23`;
+let benvenuto = `${nome}${cognome}${colore}23`;
 
 document.getElementById("ciao").innerHTML = benvenuto
 
